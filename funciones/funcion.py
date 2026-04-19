6a7f810d8714b59ba7da60fbae6231d59b9d136b
Romelio <joeloporta36@gmail.com>
2026-04-19 16:53:51 -0600

    ejercicio de funciones completado

--- funciones/funcion.py
+++ funciones/funcion.py
@@ -1,4 +1,13 @@
-def saludar(nombre):
-    print("Hola", nombre)
+def calcular_promedio(nota1, nota2,nota3,nota4,nota5):
+    promedio = (nota1 + nota2 + nota3 + nota4 + nota5) / 5
+    return promedio
 
-saludar("Jeremy")
+n1=float(input("Ingrese nota 1: "))
+n2=float(input("Ingrese nota 2: "))
+n3=float(input("Ingrese nota 3: "))
+n4=float(input("Ingrese nota 4: "))
+n5=float(input("Ingrese nota 5: "))
+
+resultado = calcular_promedio(n1,n2,n3,n4,n5)
+
+print("El promedio es: ", resultado)
